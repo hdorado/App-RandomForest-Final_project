c5261850f3ca877d252d6e2a4614c8b278f9e478
unknown <HADORADO@ciatco00127l.CGIARAD.ORG>
2016-01-25 16:48:07 -0500

    upadate

--- ui.R
+++ ui.R
@@ -15,10 +15,13 @@ shinyUI(
                                  for multivariate data:")),
                br(),
                
-               p("This application is for exploring data sets using random forest models. 
-                 Once the user has loaded a tidy database, 
-                 immediately can start to explore the correlation between two 
-                 variables and identify the subset of relevance factors."),
+               p("This application is for exploring data sets with numeric targets using bivariate analysis
+                 and the variable importance for  Random Forest."),
+             br(),
+             p("The user can select the data set that want to explore, there are
+               three possibilities. If he want to use other database, he can
+               save it into the folder using the same structure of the others. 
+               Remember that the file have to be in csv format."),
             br()     
           )
         ),
@@ -33,9 +36,15 @@ shinyUI(
         
         fluidRow(
             column(12,
+                   p("The first five row from the dataset are:",align="center"),
                 div(tableOutput("baseDatos"),align="center"),
             h3(strong('Exploratory data analysis'),align = "center" ),
             br(),
+            p("A scatterplot for quantitative variable and a box plot for 
+              qualitative variables, could be generated. 
+              The user can select the features that want to include in the graph,
+              when both x and y are  quantitative a Lowess smoother is added to
+              the graphic."),
             br()
             )        
         ),
@@ -51,6 +60,11 @@ shinyUI(
             column(12,
                 plotOutput("plotDescrip"),
                 h3(strong("Importance relevance\nRandom Forest"),align="center"),
+                br(),
+                p('You can appreciate the order of relevant inputs features to 
+                  explain the output in the graph below. Higher is the mean 
+                  variable importance, most relevant  is for the output.'),
+                
                 plotOutput("plotRelevance")
             )
         )
